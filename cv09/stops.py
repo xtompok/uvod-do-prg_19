c11bd97c55ff185a21046976d639c1ff66976b28
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2019-12-10 10:10:42 +0100

    Zdrojaky z 9. cviceni.

--- cv09/stops.py
+++ cv09/stops.py
@@ -7,8 +7,16 @@ print(data)
 print(data['features'][0]['properties']['ZAST_NAZEV'])
 print(type(data['features']))
 
-print(len(data['features']))
-
 for feat in data['features']:
     print(feat)
     print(feat['properties']['OBJECTID'])
+
+filtered_stops = []
+for feat in data['features']:
+    if feat['properties']['ZAST_PASMO'] == 'P':
+        filtered_stops.append(feat)
+
+out_geojson = {'type': 'FeatureCollection'}
+out_geojson['features'] = filtered_stops
+with open("stops_out.json","w",encoding="utf-8") as f:
+    json.dump(out_geojson, f, indent=2, ensure_ascii=False)
